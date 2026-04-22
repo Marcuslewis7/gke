2ae3764c11fa23c6c15f13dfb6cace9c3dfcd5f1
Marcuslewis7 <33000183+Marcuslewis7@users.noreply.github.com>
2026-04-22 15:26:24 +0100

    Add Kubernetes provider configuration with GCP credentials

--- provider.tf
+++ provider.tf
@@ -18,3 +18,12 @@ provider "google" {
   project = var.project_id
   region  = var.region
 }
+
+# Get current GCP credentials
+data "google_client_config" "default" {}
+
+provider "kubernetes" {
+  host                   = "https://${google_container_cluster.primary.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
+}
